888e1802a64543cebf3a7d754f426d9c0e5d0059
ryangraham <ryangrahamwpb@gmail.com>
2017-04-04 11:19:07 -0400

    Adjust css for slider functionality.

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,8 +1,8 @@
 
-$('.slider').slick({
+$('.slider-area').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     dots: true,
     arrows: false,
     autoplaySpeed: 7000,
